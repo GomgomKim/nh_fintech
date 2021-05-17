45a6eb23e844f7f49eafb5628f7fc392c4795099
hyukiii <ftkem2003@nnbox.co.kr>
2021-05-17 10:54:56 +0900

    TimeDelayDialog 수정

--- src/components/dialog/order/TimeDelayDialog.js
+++ src/components/dialog/order/TimeDelayDialog.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { Form, Button, Checkbox, Select } from "antd";
-import "../../../css/modal.css";
-import { ClockCircleOutlined } from "@ant-design/icons";
-import { connect } from "react-redux";
-import { httpPost, httpUrl } from "../../../api/httpClient";
+import React, { Component } from 'react';
+import { Form, Button, Checkbox, Select } from 'antd';
+import '../../../css/modal.css';
+import { ClockCircleOutlined } from '@ant-design/icons';
+import { connect } from 'react-redux';
+import { httpPost, httpUrl } from '../../../api/httpClient';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -17,62 +17,62 @@ class TimeDelayDialog extends Component {
       btnInfos: [
         {
           value: 5,
-          text: "5분",
+          text: '5분',
           toggle: true,
         },
         {
           value: 10,
-          text: "10분",
+          text: '10분',
           toggle: true,
         },
         {
           value: 15,
-          text: "15분",
+          text: '15분',
           toggle: true,
         },
         {
           value: 20,
-          text: "20분",
+          text: '20분',
           toggle: true,
         },
         {
           value: 30,
-          text: "30분",
+          text: '30분',
           toggle: true,
         },
         {
           value: 40,
-          text: "40분",
+          text: '40분',
           toggle: true,
         },
         {
           value: 1005,
-          text: "후 5분",
+          text: '후 5분',
           toggle: true,
         },
         {
           value: 1010,
-          text: "후 10분",
+          text: '후 10분',
           toggle: true,
         },
       ],
     };
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({
       deliveryNotAvailable: e.target.checked,
     });
   };
 
-  handleClick = (value) => {
+  handleClick = value => {
     this.setState({
       delayTime: value,
     });
   };
 
-  handleToggle = (value) => {
-    const toggledBtnInfos = this.state.btnInfos.map((btnInfo) => {
+  handleToggle = value => {
+    const toggledBtnInfos = this.state.btnInfos.map(btnInfo => {
       if (btnInfo.value === value) {
         return {
           value: value,
@@ -93,31 +93,31 @@ class TimeDelayDialog extends Component {
       httpPost(httpUrl.updateBranch, [], {
         idx: this.props.branchIdx,
         deliveryEnabled: !this.state.deliveryNotAvailable,
-        pickupAvTime10: btnInfos.find((e) => e.value === 10).toggle,
-        pickupAvTime10After: btnInfos.find((e) => e.value === 1010).toggle,
-        pickupAvTime15: btnInfos.find((e) => e.value === 15).toggle,
-        pickupAvTime20: btnInfos.find((e) => e.value === 20).toggle,
-        pickupAvTime30: btnInfos.find((e) => e.value === 30).toggle,
-        pickupAvTime40: btnInfos.find((e) => e.value === 40).toggle,
-        pickupAvTime5: btnInfos.find((e) => e.value === 5).toggle,
+        pickupAvTime10: btnInfos.find(e => e.value === 10).toggle,
+        pickupAvTime10After: btnInfos.find(e => e.value === 1010).toggle,
+        pickupAvTime15: btnInfos.find(e => e.value === 15).toggle,
+        pickupAvTime20: btnInfos.find(e => e.value === 20).toggle,
+        pickupAvTime30: btnInfos.find(e => e.value === 30).toggle,
+        pickupAvTime40: btnInfos.find(e => e.value === 40).toggle,
+        pickupAvTime5: btnInfos.find(e => e.value === 5).toggle,
         pickupAvTime50: true,
-        pickupAvTime5After: btnInfos.find((e) => e.value === 1005).toggle,
+        pickupAvTime5After: btnInfos.find(e => e.value === 1005).toggle,
         pickupAvTime60: true,
         pickupAvTime70: true,
       })
-        .then((res) => {
-          if (res.result === "SUCCESS") {
-            alert("성공적으로 처리되었습니다.");
+        .then(res => {
+          if (res.result === 'SUCCESS') {
+            alert('성공적으로 처리되었습니다.');
           } else {
-            alert("res는 왔는데 result가 SUCCESS가 아닌 경우.");
+            alert('res는 왔는데 result가 SUCCESS가 아닌 경우.');
           }
         })
-        .catch((e) => {
+        .catch(e => {
           console.log(e);
-          alert("처리가 실패했습니다.");
+          alert('처리가 실패했습니다.');
         });
     } else {
-      alert("지점을 선택해주세요!");
+      alert('지점을 선택해주세요!');
     }
   };
 
@@ -135,12 +135,12 @@ class TimeDelayDialog extends Component {
                 <div className="timeDelay-title">호출설정</div>
                 <img
                   onClick={close}
-                  src={require("../../../img/login/close.png").default}
+                  src={require('../../../img/login/close.png').default}
                   className="timeDelay-close"
                 />
                 <div className="timeDelay-inner">
                   <div className="timeDelay-box">
-                    {btnInfos.map((btnInfo) => {
+                    {btnInfos.map(btnInfo => {
                       return (
                         <Button
                           icon={
@@ -150,8 +150,8 @@ class TimeDelayDialog extends Component {
                           }
                           className={
                             !this.state.deliveryNotAvailable && btnInfo.toggle
-                              ? "timeDelay-box-on"
-                              : "timeDelay-box-off"
+                              ? 'timeDelay-box-on'
+                              : 'timeDelay-box-off'
                           }
                           onClick={() => this.handleToggle(btnInfo.value)}
                         >
@@ -165,7 +165,7 @@ class TimeDelayDialog extends Component {
                     <div className="timeDelay-btn">
                       <div className="timeDelay-btn-01">배달불가</div>
                       <Checkbox
-                        onChange={(e) => this.handleChange(e)}
+                        onChange={e => this.handleChange(e)}
                         style={{ marginTop: 11 }}
                       ></Checkbox>
                     </div>
@@ -192,13 +192,13 @@ class TimeDelayDialog extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     branchIdx: state.login.loginInfo.userGroup,
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {};
 };
 
