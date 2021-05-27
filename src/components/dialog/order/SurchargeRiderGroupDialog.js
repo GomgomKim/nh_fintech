ea591f6a7b99e07850dccf2b2656f8ea6768bae4
lee deok ho <dbrwh35@nate.com>
2021-05-27 21:27:28 +0900

    할증 그룹등록, 가맹점추가 dialog ui 작업및 그룹목록 api 작업

---
+++ src/components/dialog/order/SurchargeRiderGroupDialog.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import {
+    Form, Input, Button,
+} from "antd";
+import '../../../css/modal.css';
+const FormItem = Form.Item;
+
+class SurchargeRiderGroupDialog extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [],
+            pagination: {
+                total: 0,
+                current: 1,
+                pageSize: 5,
+            },
+            staffAuth: 1,
+            selRiderList: [],
+        };
+        this.formRef = React.createRef();
+    }
+
+    componentDidMount() {
+    }
+
+    onChange = e => {
+        // console.log('radio checked', e.target.value);
+        this.setState({
+            staffAuth: e.target.value,
+        });
+    };
+
+    handleTableChange = (pagination) => {
+        console.log(pagination)
+        const pager = { ...this.state.pagination };
+        pager.current = pagination.current;
+        pager.pageSize = pagination.pageSize
+        this.setState({
+            pagination: pager,
+        }, () => this.getList());
+    };
+
+    render() {
+        const { isOpen, close } = this.props;
+
+
+        return (
+            <React.Fragment>
+                {
+                    isOpen ?
+                        <React.Fragment>
+                            <div className="surchargeGroup-Dialog-overlay" onClick={close} />
+                            <div className="surchargeGroup-Dialog">
+                                <div className="surchargeGroup-content">
+                                    <div className="surchargeGroup-title">
+                                        그룹 추가
+                                    </div>
+                                    <img onClick={close} src={require('../../../img/login/close.png').default} className="addRider-close" />
+
+
+                                    <Form ref={this.formRef} onFinish={this.handleIdSubmit}>
+                                        <div className="surchargeGrouplayout">
+                                            <div className="surchargeGroupWrapper">
+                                                <div className="contentBlock">
+                                                    <div className="mainTitle">
+                                                        그룹명
+                                                    </div>
+                                                    <FormItem
+                                                        name="groupName"
+                                                        className="selectItem"
+                                                    >
+                                                        <Input placeholder="그룹명을 입력해 주세요." className="override-input">
+                                                        </Input>
+                                                    </FormItem>
+                                                </div>
+
+                                                <div className="submitBlock">
+                                                    <Button type="primary" htmlType="submit">
+                                                        등록하기
+                                                    </Button>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                    </Form>
+                                </div>
+                            </div>
+                        </React.Fragment>
+                        :
+                        null
+                }
+            </React.Fragment>
+        )
+    }
+}
+
+export default (SurchargeRiderGroupDialog);
