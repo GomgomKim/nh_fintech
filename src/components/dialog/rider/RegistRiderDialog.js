405d09869d0a8e4a380158f954a8495589ab0542
Kimminwoo <saminwoo@gmail.com>
2021-07-06 23:05:47 +0900

    패스워드는 필수, 바이크는 선택으로

--- src/components/dialog/rider/RegistRiderDialog.js
+++ src/components/dialog/rider/RegistRiderDialog.js
@@ -109,7 +109,7 @@ class RegistRiderDialog extends Component {
             riderSettingGroup: {
               idx: self.formRef.current.getFieldValue("riderSettingGroup"),
             },
-            bikeIdx: self.state.selectedBike.idx,
+            bikeIdx: self.state.selectedBike && self.state.selectedBike.idx,
             items: self.formRef.current.getFieldValue("items").join(","),
             joinDate: formatDateToDay(
               self.formRef.current.getFieldValue("joinDate")
@@ -126,7 +126,7 @@ class RegistRiderDialog extends Component {
             riderSettingGroup: {
               idx: self.formRef.current.getFieldValue("riderSettingGroup"),
             },
-            bikeIdx: self.state.selectedBike.idx,
+            bikeIdx: self.state.selectedBike && self.state.selectedBike.idx,
             items: self.formRef.current.getFieldValue("items").join(","),
             joinDate: formatDateToDay(
               self.formRef.current.getFieldValue("joinDate")
@@ -155,7 +155,7 @@ class RegistRiderDialog extends Component {
               idx: self.formRef.current.getFieldValue("riderSettingGroup"),
             },
             userType: 1,
-            bikeIdx: self.state.selectedBike.idx,
+            bikeIdx: self.state.selectedBike && self.state.selectedBike.idx,
             ncash: 0,
             items: self.formRef.current.getFieldValue("items").join(","),
             joinDate: formatDateToDay(
@@ -175,7 +175,7 @@ class RegistRiderDialog extends Component {
               idx: self.formRef.current.getFieldValue("riderSettingGroup"),
             },
             userType: 1,
-            bikeIdx: self.state.selectedBike.idx,
+            bikeIdx: self.state.selectedBike && self.state.selectedBike.idx,
             items: self.formRef.current.getFieldValue("items").join(","),
             joinDate: formatDateToDay(
               self.formRef.current.getFieldValue("joinDate")
@@ -425,7 +425,7 @@ class RegistRiderDialog extends Component {
                     </div>
                     <div className="contentBlock">
                       <div className="mainTitle">패스워드</div>
-                      <FormItem name="password" className="selectItem">
+                      <FormItem name="password" className="selectItem" rules={[{ required: true, message: "패스워드를 입력해주세요" }]}>
                         <Input.Password
                           placeholder="패스워드를 입력해 주세요."
                           className="override-input"
@@ -484,12 +484,6 @@ class RegistRiderDialog extends Component {
                       <FormItem
                         name="bikeName"
                         className="selectItem override-input"
-                        rules={[
-                          {
-                            required: true,
-                            message: "바이크를 선택해주세요",
-                          },
-                        ]}
                       >
                         <Input
                           value={
