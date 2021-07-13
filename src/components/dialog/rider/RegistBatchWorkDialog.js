3295ba30a72f87c468758c43d937591da9f8615e
jeong100 <undernet001@blict.kr>
2021-07-13 09:44:32 +0900

    일차감 등록 하단 텍스트 추가

--- src/components/dialog/rider/RegistBatchWorkDialog.js
+++ src/components/dialog/rider/RegistBatchWorkDialog.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import "../../../css/modal.css";
 import moment from 'moment';
 import { httpPost, httpUrl } from "../../../api/httpClient";
-import { customAlert, customError} from "../../../api/Modals";
+import { customAlert, customError } from "../../../api/Modals";
 
 const FormItem = Form.Item;
 // const Search = Input.Search;
@@ -46,8 +46,8 @@ class RegistBatchWorkDialog extends Component {
 
   onChangeDate = (dateString) => {
     this.setState({
-        startDate: dateString != null ? moment(dateString[0]).format('YYYY-MM-DD 00:00') : '',
-        endDate: dateString != null ? moment(dateString[1]).format('YYYY-MM-DD 23:59') : '',
+      startDate: dateString != null ? moment(dateString[0]).format('YYYY-MM-DD 00:00') : '',
+      endDate: dateString != null ? moment(dateString[1]).format('YYYY-MM-DD 23:59') : '',
     })
   };
 
@@ -58,19 +58,19 @@ class RegistBatchWorkDialog extends Component {
       endDate: this.state.endDate,
       category: 1,
       memo: '',
-    }).then((res)=>{
-      if(res.data === "SUCCESS" && res.result === "SUCCESS"){
-         customAlert("일차감 등록", 
-         this.formRef.current.getFieldValue("title") + " 일차감이 등록되었습니다.")
-         this.props.close()
-         this.props.callback()
+    }).then((res) => {
+      if (res.data === "SUCCESS" && res.result === "SUCCESS") {
+        customAlert("일차감 등록",
+          this.formRef.current.getFieldValue("title") + " 일차감이 등록되었습니다.")
+        this.props.close()
+        this.props.callback()
       }
-      else 
+      else
         customError("추가 오류", "오류가 발생하였습니다. 다시 시도해 주십시오.")
     })
-    .catch((error) => {
-      customError("추가 오류", "오류가 발생하였습니다. 다시 시도해 주십시오.")
-    });
+      .catch((error) => {
+        customError("추가 오류", "오류가 발생하였습니다. 다시 시도해 주십시오.")
+      });
   }
 
 
@@ -83,48 +83,48 @@ class RegistBatchWorkDialog extends Component {
         <div className="taskWork-Dialog">
           <div className="taskWork-content">
             <Form ref={this.formRef} onFinish={this.handleSubmit}>
-            <div className="taskWork-title">일차감 등록</div>
-            <img
-              onClick={close}
-              src={require("../../../img/login/close.png").default}
-              className="taskWork-close"
-              alt="close"
-            />
-            <div className="taskWork-title-sub">일차감 등록정보</div>
-            <div className="taskWork-inner">
-              <div className="taskWork-list">
-                {/* <div className="twl taskWork-list-01">
+              <div className="taskWork-title">일차감 등록</div>
+              <img
+                onClick={close}
+                src={require("../../../img/login/close.png").default}
+                className="taskWork-close"
+                alt="close"
+              />
+              <div className="taskWork-title-sub">일차감 등록정보</div>
+              <div className="taskWork-inner">
+                <div className="taskWork-list">
+                  {/* <div className="twl taskWork-list-01">
                   <div className="twl-text">사용여부</div>
                   <Checkbox></Checkbox>
                   {/* <span className="useText">사용함</span> */}
-                {/* </div> */}
-                <div className="twl taskWork-list-02">
-                  <div className="twl-text">차감명</div>
-                  <div className="inputBox inputBox-taskWork sub">
-                    <FormItem
-                      name="title"
-                      rules={[{ required: true, message: "차감명을 입력해주세요." }]}
-                    >
-                      <Input style={{ width:250 }}/>
-                    </FormItem>
-                  </div>
-                </div>
-                <div className="twl taskWork-list-04">
-                  <div className="twl-text">차감금액</div>
-                  <div className="taskWork-place1">
+                  {/* </div> */}
+                  <div className="twl taskWork-list-02">
+                    <div className="twl-text">차감명</div>
                     <div className="inputBox inputBox-taskWork sub">
                       <FormItem
-                        name="ncashDelta"
-                        rules={[{ required: true, message: "금액을 입력해주세요." }]}
+                        name="title"
+                        rules={[{ required: true, message: "차감명을 입력해주세요." }]}
                       >
-                        <Input style={{ width:250, marginRight: 10, float: "left" }} />
+                        <Input style={{ width: 250 }} />
                       </FormItem>
                     </div>
                   </div>
-                </div>
+                  <div className="twl taskWork-list-04">
+                    <div className="twl-text">차감금액</div>
+                    <div className="taskWork-place1">
+                      <div className="inputBox inputBox-taskWork sub">
+                        <FormItem
+                          name="ncashDelta"
+                          rules={[{ required: true, message: "금액을 입력해주세요." }]}
+                        >
+                          <Input style={{ width: 250, marginRight: 10, float: "left" }} />
+                        </FormItem>
+                      </div>
+                    </div>
+                  </div>
 
 
-                {/* <div className="twl taskWork-list-02">
+                  {/* <div className="twl taskWork-list-02">
                   <div className="twl-text">메모</div>
                   <div className="inputBox inputBox-taskWork sub">
                     <FormItem
@@ -136,7 +136,7 @@ class RegistBatchWorkDialog extends Component {
                 </div> */}
 
 
-                {/* <div className="twl taskWork-list-03">
+                  {/* <div className="twl taskWork-list-03">
                   <div className="twl-text">적용대상</div>
                   <div className="taskWork-place1">
                   {this.state.batchSearchGrp &&
@@ -159,8 +159,8 @@ class RegistBatchWorkDialog extends Component {
                     </FormItem>                                             
                   </div>
                 </div> */}
-                <div className="twl taskWork-list-05">
-                  <div className="twl-text">기간제한</div>
+                  <div className="twl taskWork-list-05">
+                    <div className="twl-text">기간제한</div>
                     <RangePicker
                       placeholder={['시작일', '종료일']}
                       // showTime={{ format: 'HH:mm' }}
@@ -168,19 +168,26 @@ class RegistBatchWorkDialog extends Component {
                       disabled={this.state.disabled}
                       style={{ width: 350 }}
                     />
+                  </div>
+                  <div style={{
+                    lineHeight: 1,
+                    color: "#ccc",
+                    textAlign: "center",
+                    marginLeft: -25
+                  }}>
+                    * 선택한 기간동안 새벽 5시에 차감됩니다
+                  </div>
                 </div>
               </div>
 
-            </div>
-
-            <div className="taskWork-btn-01">
-              <Button 
-                type="primary" htmlType="submit"
-                className="tabBtn taskWork-btn">
-                등록
-              </Button>
-            </div>
-          </Form>
+              <div className="taskWork-btn-01">
+                <Button
+                  type="primary" htmlType="submit"
+                  className="tabBtn taskWork-btn">
+                  등록
+                </Button>
+              </div>
+            </Form>
           </div>
         </div>
       </React.Fragment>
