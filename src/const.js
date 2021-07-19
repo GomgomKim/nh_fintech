9522f98c80d2971beff70cd99cd037ecb1fc38d4
@babbbbly <babbbbly@gmail.com>
2021-07-19 14:09:50 +0900

    모바일버전start

--- src/const.js
+++ src/const.js
@@ -2,21 +2,21 @@ const Const = {
   appName: "nnboxPartner",
 
   //local
-  // serverProtocol: "http",
-  // serverIp: "127.0.0.1",
+  serverProtocol: "http",
+  serverIp: "127.0.0.1",
 
   //stage
   // serverProtocol: "https",
   // serverIp: "nnstage.nnbox.net",
 
   //prod
-  serverProtocol: "https",
-  serverIp: "rider.nyamnyambox.com",
+  // serverProtocol: "https",
+  // serverIp: "rider.nyamnyambox.com",
 
   //drnana
   // serverProtocol: "https",
   // serverIp: "drnana.nnbox.net",
-  
+
   serverPort: "30001",
 
   //naver map client key
